f5b810ee0b097b1b52868250812d83779bed96e3
Kira <kiratechno@protonmail.com>
2019-05-01 18:50:39 +0300

    new file:   hello.py

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
 sudo ln -sf ~/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+
+gunicorn -b '0.0.0.0:8080' hello:app &
